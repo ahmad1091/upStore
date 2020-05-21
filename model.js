bb80bd1f31ed85e6dde8f381c28f3c8d13299aa3
ahmad1091 <ahmadtayeb91@gmail.com>
2020-05-21 07:09:37 +0300

    add the model file which contains the data

---
+++ model.js
@@ -0,0 +1,99 @@
+var products = [
+{
+	name:'Bugatti, La Voiture Noire',
+	price:'$19 million',
+	img:'imges/Bugatti.jpg',
+	shipping:'free',
+	quantityAvailable:1,
+	category:'cars'
+},
+{
+	name:'Audi r8',
+	price:'$208,100',
+	img:'imges/Audi.jpg',
+	shipping:'free',
+	quantityAvailable:50,
+	category:'cars'
+},
+{
+	name:'Dodge challenger red eye',
+	price:'$70,000',
+	img:'imges/dodge.jpg',
+	shipping:'free',
+	quantityAvailable:90,
+	category:'cars'
+}
+,
+{
+	name:'Jeans',
+	price:'$70',
+	img:'imges/jeans.png',
+	shipping:'$3',
+	quantityAvailable:500,
+	category:'fashion'
+},
+{
+	name:'Shirt',
+	price:'$50',
+	img:'imges/fitch.png',
+	shipping:'$12',
+	quantityAvailable:600,
+	category:'fashion'
+},
+{
+	name:'Sneaker',
+	price:'$50',
+	img:'imges/Sneaker.jpg',
+	shipping:'$12',
+	quantityAvailable:600,
+	category:'fashion'
+},
+{
+	name:'Laptop',
+	price:'$1200',
+	img:'imges/mac.jpg',
+	shipping:'$30',
+	quantityAvailable:8,
+	category:'electronics'
+},
+{
+	name:'Mobile',
+	price:'$800',
+	img:'imges/moblie.jpg',
+	shipping:'$40',
+	quantityAvailable:100,
+	category:'electronics'
+},
+{
+	name:'Watch',
+	price:'$800',
+	img:'imges/Watch.jpg',
+	shipping:'$40',
+	quantityAvailable:100,
+	category:'electronics'
+},
+{
+	name:'Coffee table',
+	price:'$80',
+	img:'imges/table.jpg',
+	shipping:'free',
+	quantityAvailable:36,
+	category:'furniture'
+},
+{
+	name:'sofa',
+	price:'$80',
+	img:'imges/sofa.jpg',
+	shipping:'free',
+	quantityAvailable:36,
+	category:'furniture'
+},
+{
+	name:'chair',
+	price:'$80',
+	img:'imges/chair.jpeg',
+	shipping:'free',
+	quantityAvailable:36,
+	category:'furniture'
+}
+]
